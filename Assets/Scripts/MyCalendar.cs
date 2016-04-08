adfc4868ad45bfcc23ca7e05bc78b3488c00aef8
moritanian <pickit2express@gmail.com>
2016-04-08 09:17:08 +0900

    notify-2 コンボボックス、カレンダーUI

---
+++ Assets/Scripts/MyCalendar.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Reflection;
+using System.Collections.Generic;
+using System;
+
+/*
+DayOfWeek 列挙体
+Sunday :0 ~ 6
+*/
+
+public class MyCalendar : Token {
+
+	PanelSlider _panelSlider;
+	int year;
+	int month;
+
+	// Use this for initialization
+	void Start () {
+		_panelSlider = GetComponent<PanelSlider>();
+		year = DateTime.Now.Year;
+		month = DateTime.Now.Month;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void GoCal(){
+		_panelSlider.SlideIn();
+
+	}
+	
+	public void ExitCal(){
+		_panelSlider.SlideOut();
+	}
+
+	public void GetMonthData(int year, int month){	
+		int days_in_month = DateTime.DaysInMonth(year, month);
+		DateTime first_day = new DateTime(year, month, 1);
+		DayOfWeek week = first_day.DayOfWeek;
+	}
+}
