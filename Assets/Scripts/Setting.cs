d3b21a4e9b26b3ea323cd44d9b05dd3cf1fa79fc
moritanian <pickit2express@gmail.com>
2017-12-27 03:12:06 +0900

    nativeEdeitText 取り込み

--- Assets/Scripts/Setting.cs
+++ Assets/Scripts/Setting.cs
@@ -241,9 +241,9 @@ public class Setting : Token {
     	GetComponent<Image>().color = colors[4];
     	MyCanvas.Find<Image>("ScrollView").color = colors[2];
     	MyCanvas.Find<Image>("BoardMain").color = colors[5];
-    	MyCanvas.Find<Image>("TextInput").color = colors[4];
-    	MyCanvas.Find<Text>("TodoTextField").color = colors[1];
-    	MyCanvas.Find<Text>("DispDateTimeText").color = colors[1];
+		MyCanvas.Find<Image>("TextInput").color = colors[4];
+		MyCanvas.Find<Text>("TodoTextField").color = colors[1];
+		MyCanvas.Find<Text>("DispDateTimeText").color = colors[1];
     	MyCanvas.Find<Image>("SearchField").color = colors[3];
     	MyCanvas.Find<Text>("SearchText").color = colors[1];
     	MyCalendar myCalendar = MyCanvas.Find<MyCalendar>("MyCalendar");
